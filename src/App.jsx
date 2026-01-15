750e977f5debaf3ce1822d8fab1dc750f80f433f
ydaqqa05 <yomnamdaqqa05@gmail.com>
2026-01-16 00:10:02 +0200

    portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import Navbar from "./navbar/Navbar"
 import Hero from "./hero/Hero"
+import Portfolio from "./portfolio/Portfolio"
 function App(){
   return <>
   <Navbar/>
   <Hero/>
+  <Portfolio/>
   </>
 }
 export default App
---
+++ src/portfolio/Portfolio.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import style from './portfolio.module.css'
+import Image1 from './../assets/imgs/cabin.png'
+import Image2 from './../assets/imgs/cake.png'
+import Image3 from './../assets/imgs/circus.png'
+import Image4 from './../assets/imgs/game.png'
+import Image5 from './../assets/imgs/safe.png'
+import Image6 from './../assets/imgs/submarine.png'
+export default function Portfolio() {
+  return (
+    <>
+    <div className=' pt-5'>
+        <h1 className={`${style.big_font} pt-5 text-center  `}>Portfolio</h1>
+ <div className={`${style.star_divider}`}>
+  <div className={`${style.line}`}></div>
+  <div className={`${style.star}`}>★</div>
+  <div className={`${style.line}`}></div>
+</div>
+<div className="portfolio">
+<div className="container mt-5">
+ <div className="row g-5">
+    <div className='col-md-4 '> <img src={Image1} className='img-fluid rounded-3' /></div>
+    <div className='col-md-4 '><img src={Image2} className='img-fluid  rounded-3' /></div>
+    <div className='col-md-4 '><img src={Image3} className='img-fluid rounded-3'/></div>
+    <div className='col-md-4 '><img src={Image4} className='img-fluid rounded-3' /></div>
+    <div className='col-md-4 '><img src={Image5} className='img-fluid rounded-3'/></div>
+    <div className='col-md-4 '><img src={Image6} className='img-fluid rounded-3'/></div>
+    </div>
+    </div>
+
+</div>
+    </div>
+    </>
+  )
+}
