f89fb43381fb2a241c3dfd7edff05a8ce3634df9
ydaqqa05 <yomnamdaqqa05@gmail.com>
2026-01-15 21:09:29 +0200

    navbar section

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+import Navbar from "./navbar/Navbar"
+function App(){
+  return <>
+  <Navbar/>
+  </>
+}
+export default App
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import style from './navbar.module.css'
+function Navbar() {
+    return (
+      <>
+       <nav className={`${style.bg_navbar} navbar navbar-expand-lg navbar-dark`}>
+  <div className="container-fluid py-3">
+
+    <a className="navbar-brand fw-bold fs-2 text-white ms-5" href="#">
+      Start Bootstrap
+    </a>
+
+    <button
+      className="navbar-toggler text-white"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+ 
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto me-5 gap-3">
+        <li><a className="nav-link text-white fs-5 fw-bold">Portfolio</a></li>
+        <li><a className="nav-link text-white fs-5 fw-bold">About</a></li>
+        <li><a className="nav-link text-white fs-5 fw-bold">Contact</a></li>
+      </ul>
+    </div>
+
+  </div>
+</nav>
+
+      </>
+    );
+  }
+  
+  export default Navbar;
